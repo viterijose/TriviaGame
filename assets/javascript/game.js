e8fca9cc0f77b6bd468b217cddde54a328acccf7
Jose Viteri <Jose@Joses-MBP.home>
2018-03-27 12:02:55 -0400

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+
+window.onload = function () {//initialize game once the page starts up
+    var QandA = {
+        Question_1 : "Which bear is best?",
+        Question_2 : "Bears, beets ,_____?",
+        Question_3 : "Which cat did "
+    }
+}
