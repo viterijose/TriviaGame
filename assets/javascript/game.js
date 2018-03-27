92bdb2827c4776e28aa34eb479d7f672cb0ec4cd
Jose Viteri <33139338+viterijose@users.noreply.github.com>
2018-03-27 16:00:09 -0400

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,60 @@
 
 window.onload = function () {//initialize game once the page starts up
-    var QandA = {
-        Question_1 : "Which bear is best?",
-        Question_2 : "Bears, beets ,_____?",
-        Question_3 : "Which cat did "
+    var QandA = [
+         ["Which bear is best?", "Bears, beets ,_____?", "Which cat did "],
+        ["Black bear", "Brown bear", "Polar bear", "Panda bear"],
+         ["Battlestar galactica", "Bears", "Stanley", "Recyclops"],
+        ["Fluffler", "Mr wiggle", "Nutterball", "Benedict"]
+    ]
+    var score = 0;
+    var wrong = 0;
+    var select_question;
+    var select_answer;
+    var selected;
+    var question;
+    var answer;
+
+    $(".start").on("click", function () {
+        //begin timer
+
+    })
+    function Questions() {
+        selected = Math.round(Math.random() * QandA.length);//select a random question from array
+        select_question = QandA[0];
+        console.log(select_question);
+        question = select_question[selected];
+        $(".question").text(question);//change text in question div
+        console.log("Selected question :" + question);
+        select_answer = QandA [selected + 1];
+        console.log("Array of answers :" + select_answer);
+        answer = select_answer[0];
+        console.log("Answer to question :" + answer);
+        shuffleArray(select_answer);
+        
     }
+
+    Questions();
+
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        $(".A").text(select_answer[0]);
+        $(".B").text(select_answer[1]);
+        $(".C").text(select_answer[2]);
+        $(".D").text(select_answer[3]);
+    }
+
+    $("button").on("click", function(){
+        console.log("You chose  : "+$(this).text());
+        console.log("The answer : "+answer)
+        if ($(this).text() == answer){
+            alert("You got it");
+        }else{
+            alert("Wrong");
+        }
+    })
 }
